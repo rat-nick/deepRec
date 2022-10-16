f145f7d6b116fda77a2161eb8175e10806f6410c
ratinac-nikola <ratinac.nikola@gmail.com>
2022-10-16 20:10:46 +0200

    add CUDA capabilites

--- recommender/utils/data.py
+++ recommender/utils/data.py
@@ -9,5 +9,7 @@ def ratingsToTensor(trainset: Trainset) -> torch.Tensor:
 
     for u, i, r in trainset.all_ratings():
         t[int(u)][int(i)][int(r) - 1] = 1.0
-
+    if torch.cuda.is_available():
+        t = t.to("cuda")
+        print("Using CUDA!")
     return t
