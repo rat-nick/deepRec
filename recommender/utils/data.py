37bdb62656966b67875a0200112fca25e9c2fcab
ratinac-nikola <ratinac.nikola@gmail.com>
2022-10-16 20:14:09 +0200

    adding more CUDA capabilites

--- recommender/utils/data.py
+++ recommender/utils/data.py
@@ -5,11 +5,15 @@ import torch
 
 def ratingsToTensor(trainset: Trainset) -> torch.Tensor:
     scale = trainset.rating_scale[1] - trainset.rating_scale[0] + 1
-    t = torch.zeros(trainset.n_users, trainset.n_items, scale)
+
+    if torch.cuda.is_available():
+        print("CUDA is available!")
+        t = torch.zeros(trainset.n_users, trainset.n_items, scale, device="cuda")
+    else:
+        print("CUDA is NOT available!")
+        t = torch.zeros(trainset.n_users, trainset.n_items, scale)
 
     for u, i, r in trainset.all_ratings():
         t[int(u)][int(i)][int(r) - 1] = 1.0
-    if torch.cuda.is_available():
-        t = t.to("cuda")
-        print("Using CUDA!")
+
     return t
