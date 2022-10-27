b6b1082624031f10248acef5513ace414fb7d460
ratinac-nikola <ratinac.nikola@gmail.com>
2022-10-27 09:38:54 +0000

    fix batching

--- data/dataset.py
+++ data/dataset.py
@@ -1,3 +1,4 @@
+from os import link
 from random import shuffle
 from pathlib import Path
 import pandas as pd
@@ -5,6 +6,7 @@ import torch
 from sklearn.model_selection import train_test_split
 from surprise import Dataset, Reader
 from surprise.dataset import Trainset
+import time
 
 DATA_DIR = Path(__file__).parent / "ml-20m"
 RATINGS_FILE = "ratings.csv"
@@ -13,28 +15,60 @@ LINKS_FILE = "links.csv"
 
 
 class MyDataset:
-    def __init__(self):
+    def __init__(
+        self,
+        data_dir="ml-20m",
+        ratings_path="ratings.csv",
+        ratings_sep=",",
+        items_path="movies.csv",
+        items_sep=",",
+        links_path="",
+        links_sep=",",
+    ):
+        data_dir = Path(__file__).parent / data_dir
         if torch.cuda.is_available():
-            print("CUDA available!")
+            print("CUDA available! Setting default tensor type to cuda.FloatTensor")
             torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
-        self.rawRatingsDF = pd.read_csv(f"{DATA_DIR}/{RATINGS_FILE}", sep=",", header=0)
-        self.itemsDF = pd.read_csv(f"{DATA_DIR}/{ITEMS_FILE}", sep=",", header=0)
-        linksDF = pd.read_csv(f"{DATA_DIR}/{LINKS_FILE}", sep=",", header=0)
+        self.rawRatingsDF = pd.read_csv(
+            f"{data_dir}/{ratings_path}",
+            sep=ratings_sep,
+            header=0,
+            engine="python",
+            encoding="latin-1",
+        )
+        self.itemsDF = pd.read_csv(
+            f"{data_dir}/{items_path}",
+            sep=items_sep,
+            header=0,
+            engine="python",
+            encoding="latin-1",
+        )
 
-        linksDF = linksDF.fillna(0)
-        linksDF["tmdbId"] = linksDF["tmdbId"].astype("int")
+        if links_path != "":
+            linksDF = pd.read_csv(
+                f"{data_dir}/{links_path}",
+                sep=links_sep,
+                header=0,
+                engine="python",
+                encoding="latin-1",
+            )
 
-        self.itemsDF = pd.concat([self.itemsDF, linksDF], axis=1, join="inner")
-        self.itemsDF = self.itemsDF.loc[:, ~self.itemsDF.T.duplicated(keep="first")]
+            linksDF = linksDF.fillna(0)
+            linksDF["tmdbId"] = linksDF["tmdbId"].astype("int")
 
+            self.itemsDF = pd.concat([self.itemsDF, linksDF], axis=1, join="inner")
+            self.itemsDF = self.itemsDF.loc[:, ~self.itemsDF.T.duplicated(keep="first")]
+
+        self.rawRatingsDF.columns = ["user", "item", "rating", "timestamp"]
         self.rawRatingsDF["rating"] *= 2
         self.rawRatingsDF = self.rawRatingsDF.rename(
             columns={"userId": "user", "movieId": "item"}
         )
-
-        self.rawRatingsDF.drop("timestamp", inplace=True, axis=1)
-
+        try:
+            self.rawRatingsDF.drop("timestamp", inplace=True, axis=1)
+        except:
+            print("No timestamp...")
         self.ratingsDS = Dataset.load_from_df(
             df=self.rawRatingsDF,
             reader=Reader(
@@ -102,30 +136,42 @@ class Batcher:
         self.size = size
 
     def next(self):
+        # TODO: optimize batching process
+
         users = self.df["user"].unique()
         shuffle(users)
 
+        # initialize the tensor to be returned
         t = torch.zeros((self.bs,) + self.size)
 
         current = 0
         for u in users:
-            userRatings = self.df[self.df["user"] == u]
+            start = time.time()
+            ratings = self.df[self.df["user"] == u]
             t[
                 current,
-                userRatings["item"].to_numpy(),
-                userRatings["rating"].to_numpy() - 1,
+                ratings["item"].to_numpy(),
+                ratings["rating"].to_numpy() - 1,
             ] = 1.0
             current += 1
-            if current == self.bs:
+            if current >= self.bs:
                 yield t
+                # print(f"Batching lasted {time.time() - start}")
                 current = 0
+
         yield t
         return
 
 
 if __name__ == "__main__":
-    dataAccess = MyDataset()
+    dataAccess = MyDataset(
+        data_dir="ml-1m",
+        ratings_path="ratings.dat",
+        ratings_sep="::",
+        items_path="movies.dat",
+        items_sep="::",
+    )
     dataAccess.trainTestValidationSplit()
     trainset = dataAccess.trainset
-    for b in dataAccess.batches(dataAccess.trainData, 10):
-        print(b)
+    for b in dataAccess.batches(dataAccess.trainData, 1024):
+        pass
